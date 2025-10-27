29d75136c91052f66a5794b174f8e50f7b11c524
sachindk <sachindk52@gmail.com>
2025-10-27 23:13:30 +0530

    signup page created

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config(); 
 
 mongoose
@@ -20,3 +21,4 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRouter);
+app.use('./api/auth', authRouter);
